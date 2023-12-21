4dd4472d94acb7470efb369dea7110f820d28a30
Kenan Iusubovi <te666za@gmail.com>
2023-12-21 13:05:23 +0100

    homework done

--- src/main/java/lesson/lesson8-9(Spring_Boot)/g_27_rest_demo/src/main/java/de/aittr/g_27_rest_demo/controllers/DogController.java
+++ src/main/java/lesson/lesson8-9(Spring_Boot)/g_27_rest_demo/src/main/java/de/aittr/g_27_rest_demo/controllers/DogController.java
@@ -1,11 +1,14 @@
 package de.aittr.g_27_rest_demo.controllers;
 
+import de.aittr.g_27_rest_demo.domain.Dog;
 import de.aittr.g_27_rest_demo.domain.IDog;
 import de.aittr.g_27_rest_demo.services.DogService;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +36,12 @@ public class DogController {
   @DeleteMapping("/delete/{id}")
   public void deleteById (@PathVariable int id){
     service.deleteById(id);
+  }
+
+  @PostMapping("/save")
+  public IDog save (@RequestBody Dog obj){
+    int i = 0;
+    return service.save(obj);
   }
 
 }
